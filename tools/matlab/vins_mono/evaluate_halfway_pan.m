e6850786e57d60216c4ac465b08b821783731642
JIanzhu Huai <huai.3@osu.edu>
2020-02-28 08:53:56 +0800

    evalute panning, standstill, sideways motion with halfway panning

---
+++ tools/matlab/vins_mono/evaluate_halfway_pan.m
@@ -0,0 +1,67 @@
+function evaluate_halfway_pan(root_dir)
+% evaluate estimators' results for data sessions of sideways motion with halfway
+% panning, only panning, or only standstill.
+addpath('/media/jhuai/Seagate/jhuai/tools/export_fig/');
+% root_dit is the folder of estimator results for a batch of data.
+root_dir = '/media/jhuai/Seagate/jhuai/RSS-2020/results/hailun_house/halfway_pan_init';
+% root_dir = '/media/jhuai/Seagate/jhuai/RSS-2020/results/hailun_house/halfway_pan';
+% root_dir = '/media/jhuai/Seagate/jhuai/RSS-2020/results/hailun_house/halfway_pan_af_off';
+
+% root_dir = '/media/jhuai/Seagate/jhuai/RSS-2020/results/hailun_house/pan';
+% root_dir = '/media/jhuai/Seagate/jhuai/RSS-2020/results/hailun_house/pan_af_off';
+% root_dir = '/media/jhuai/Seagate/jhuai/RSS-2020/results/hailun_house/stationary';
+
+data_dir='/media/jhuai/OldWin8OS/jhuai/data';
+result_dir='/media/jhuai/Seagate/jhuai/RSS-2020/results';
+
+ref_dist = 5;
+msckf_results = dir([root_dir, '/**/msckf_estimates.csv']);
+vins_results = dir([root_dir, '/**/vins_result_*.csv']);
+est_results = [msckf_results; vins_results];
+
+folderlist = {est_results.folder}';
+namelist = {est_results.name}';
+durationlist = zeros(size(folderlist));
+
+% find the corresponding data session duration
+for i = 1:length(folderlist)
+    [folderpath, ~, ~] = fileparts(folderlist{i});
+    data_session_folder = strrep(folderpath, result_dir, data_dir);
+    frame_time_file = [data_session_folder, '/frame_timestamps.txt'];
+    frame_times = readmatrix(frame_time_file, 'NumHeaderLines', 1);
+    durationlist(i) = (frame_times(end) - frame_times(1)) * 0.000000001;
+    assert(durationlist(i) > 25);
+    assert(durationlist(i) < 600);
+end
+
+% compute the metrics
+output_file = [root_dir, '/metric_values.csv'];
+fileID = fopen(output_file,'w');
+fprintf(fileID, ['%%folder,estimator_file,data_uration,delta_o,range,delta_s,'...
+    'delta. The last two are irrelevant for pan and standstill data.\n']);
+for i = 1:length(folderlist)
+    if contains(namelist{i}, 'vins_result_ex.csv')
+        continue
+    end
+    if contains(folderlist{i}, 'halfway')
+        visualize_result = 1;
+    else
+        visualize_result = 0;
+    end
+    if contains(namelist{i}, 'vins_result')
+        position_indices = 2:4;
+    else
+        assert(contains(namelist{i}, 'msckf_estimates.csv'));
+        position_indices = 3:5;
+    end
+    est_file = fullfile(folderlist{i}, namelist{i});
+    fprintf('working on %s\n', est_file);
+    [delta_o, range, delta_s, delta] = metrics_line_fitting(...
+        est_file, position_indices, durationlist(i), ref_dist, ...
+        visualize_result);
+    fprintf(fileID,'%s,%s,%6.8f,%6.5f,%6.5f,%6.5f,%6.5f\n', folderlist{i}, ...
+        namelist{i}, durationlist(i), delta_o, range, delta_s, delta);
+end
+fclose(fileID);
+fprintf('The metric values are saved at %s\n', output_file);
+end
--- tools/matlab/vins_mono/metrics_line_fitting.m
+++ tools/matlab/vins_mono/metrics_line_fitting.m
@@ -1,31 +1,49 @@
-function [delta_o, delta_s, delta] = metrics_line_fitting(...
-    est_file, position_indices, data_duration, ref_length)
+function [delta_o, range, delta_s, delta] = metrics_line_fitting(...
+    est_file, position_indices, data_duration, ref_length, visualize_result)
+% input
 % data_duration in secs
 % output
 % delta_o loop error
+% range: max distance between trajectory points and the starting point
 % delta_s scale error
 % delta transverse error
-visualize_result = 1;
+
+if nargin < 5
+    visualize_result = 0;
+end
+if nargin < 4
+    ref_length = 5;
+end
+
 delta_o = 1e8;
+range=1e8;
 delta_s = 1e8;
 delta = 1e8;
-ref_length = 5;
 fid = fopen(est_file);
 firstline = fgetl(fid);
 fclose(fid);
 headerlines = 0;
+if firstline == -1
+    return;
+end
 if contains(firstline, '%') || contains(firstline, '#')
     headerlines = 1;
 end
 data = readmatrix(est_file, 'NumHeaderLines', headerlines);
+if isempty(data)
+    return;
+end
 if data(1, 1) > 1e9
     data(:, 1) = data(:, 1) * 0.000000001;
 end
 position_data = data(:, [1, position_indices]);
 if (data(end, 1) - data(1, 1)) < 0.9 * data_duration
-    return
+    return;
 end
 delta_o = sqrt(sum((position_data(end, 2:4) - position_data(1, 2:4)).^2));
+datalength = size(position_data, 1);
+range = max(sqrt(sum((position_data(:, 2:4) - repmat(position_data(1, 2:4), datalength, 1)).^2, 2)));
+
 endpoints = fit3DLine(position_data(:, 2:4)');
 [d, foot] = point_to_line(position_data(:, 2:4), endpoints(:, 1)', endpoints(:, 2)');
 dist2_to_start_projection = sum((foot - repmat(foot(1, :), size(foot, 1), 1)).^2, 2);
@@ -36,33 +54,31 @@ delta_s = abs(est_length - ref_length) / ref_length;
 delta = mean(d);
 
 if visualize_result == 1
-close all;
-
-figure;
-plot3(position_data(:, 2), position_data(:, 3), position_data(:, 4), 'b-');
-hold on;
-plot3(endpoints(1, :), endpoints(2, :), endpoints(3, :), 'k');
-% draw the estimated length projection
-plot3(foot(1, 1), foot(1, 2), foot(1, 3), 'ro');
-perp_farthest = [position_data(xindex, 2:4);...
-    foot(xindex, 1:3)];
-plot3(perp_farthest(:, 1), perp_farthest(:, 2), perp_farthest(:, 3), '-rs');
-% draw the max length perpendicular line segment
-perp = [position_data(dindex, 2:4);...
-    foot(dindex, 1:3)];
-plot3(perp(:, 1), perp(:, 2), perp(:, 3), '-b+');
-axis equal;
-grid on;
-
-xlabel('x');
-ylabel('y');
-zlabel('z');
-titlestr = 'line_fitting';
-[output_dir,name,ext] = fileparts(est_file);
-outputfig = [output_dir, '/', titlestr, '.eps'];
-if exist(outputfig, 'file')==2
-    delete(outputfig);
-end
-export_fig(outputfig);
+    close all;
+    figure;
+    plot3(position_data(:, 2), position_data(:, 3), position_data(:, 4), 'b-');
+    hold on;
+    plot3(endpoints(1, :), endpoints(2, :), endpoints(3, :), 'k');
+    % draw the estimated length projection
+    plot3(foot(1, 1), foot(1, 2), foot(1, 3), 'ro');
+    perp_farthest = [position_data(xindex, 2:4);...
+        foot(xindex, 1:3)];
+    plot3(perp_farthest(:, 1), perp_farthest(:, 2), perp_farthest(:, 3), '-rs');
+    % draw the max length perpendicular line segment
+    perp = [position_data(dindex, 2:4);...
+        foot(dindex, 1:3)];
+    plot3(perp(:, 1), perp(:, 2), perp(:, 3), '-b+');
+    axis equal;
+    grid on;
+    xlabel('x');
+    ylabel('y');
+    zlabel('z');
+    titlestr = 'line_fitting';
+    [output_dir,name,ext] = fileparts(est_file);
+    outputfig = [output_dir, '/', titlestr, '.eps'];
+    if exist(outputfig, 'file')==2
+        delete(outputfig);
+    end
+    export_fig(outputfig);
 end
 end
