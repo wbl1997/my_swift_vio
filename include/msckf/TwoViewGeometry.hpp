dc3c5fcd1bced38d67f4e993368703f02c27fdd3
JIanzhu Huai <huai.3@osu.edu>
2020-04-14 20:49:31 +0800

    compute epipolar line error in pixels

---
+++ include/msckf/TwoViewGeometry.hpp
@@ -0,0 +1,34 @@
+#ifndef INCLUDE_MSCKF_TWO_VIEW_GEOMETRY_HPP_
+#define INCLUDE_MSCKF_TWO_VIEW_GEOMETRY_HPP_
+#include <okvis/kinematics/Transformation.hpp>
+#include <Eigen/Core>
+
+namespace okvis {
+class TwoViewGeometry {
+ public:
+  static double computeErrorEssentialMat(okvis::kinematics::Transformation T_ji,
+                                         Eigen::Vector3d bearing_i,
+                                         Eigen::Vector3d bearing_j,
+                                         double focal_length);
+
+  /**
+   * @brief deviationFromEpipolarLine compute deviation in pixels from the
+   * epipolar line. see
+   * https://github.com/opencv/opencv/blob/master/modules/calib3d/src/fundam.cpp
+   * @param E_ji t_ji X R_ji. Epipolar constraint is p_j' * t_ji X R_ji * p_i = 0.
+   * @param bearing_i [x, y, 1] undistorted image coordinate at z=1 for point in
+   * image i
+   * @param bearing_j [x, y, 1] undistorted image coordinate at z=1 for point in
+   * image j
+   * @param focal_length nominal focal length to convert the epipolar line error
+   * into error of pixel unit.
+   * @return squared distance to epipolar line. Distance has a unit of pixels.
+   */
+  static double computeErrorEssentialMat(Eigen::Matrix3d E_ji,
+                                         Eigen::Vector3d bearing_i,
+                                         Eigen::Vector3d bearing_j,
+                                         double focal_length);
+};
+}  // namespace okvis
+
+#endif  // INCLUDE_MSCKF_TWO_VIEW_GEOMETRY_HPP_
