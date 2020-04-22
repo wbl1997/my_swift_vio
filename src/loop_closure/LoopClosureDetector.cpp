f4254dd2d80073edb92d4ef6811ae5b392dfaad9
JIanzhu Huai <huai.3@osu.edu>
2020-04-22 14:20:48 +0800

    fix bug

--- src/loop_closure/LoopClosureDetector.cpp
+++ src/loop_closure/LoopClosureDetector.cpp
@@ -576,7 +576,7 @@ bool LoopClosureDetector::geometricVerificationCheck(
         }
     }
   }
-  return (*loopFrameAndMatches) == nullptr;
+  return ((*loopFrameAndMatches) != nullptr);
 }
 
 void LoopClosureDetector::createMatchedKeypoints(
