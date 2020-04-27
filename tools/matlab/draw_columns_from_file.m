ed31bcac76fdd53f77a5aed06c0d52e7f1800b2a
JIanzhu Huai <huai.3@osu.edu>
2020-04-27 20:24:25 +0800

    generalize draw_columns_from_file

--- tools/matlab/draw_columns_from_file.m
+++ tools/matlab/draw_columns_from_file.m
@@ -1,10 +1,14 @@
-function draw_columns_from_file(data_file, indices, plot3d)
+function draw_columns_from_file(data_file, indices, plot3d, line_styles)
 % draw columns of data loaded from data_file
 % the first row of the data file will be removed as a header
 % the first column serves as the x axis
 % indices are range of columns to draw, 1 based, e.g., 2:4
 % plot3d if 1, plot3 will be used for the first 3 dim of indices,
 % default 0
+% line_styles line styles for each dim.
+if nargin < 4
+    line_styles = {'-r', '-g', '-b', '-k', '.k', '.b', '-c', '-m', '-y'};
+end
 if nargin < 3
     plot3d = 0;
 end
@@ -13,11 +17,8 @@ if data(1, 1) > 1e9
     data(:, 1) = data(:, 1) * 0.000000001;
 end
 data(:, 1) = data(:, 1) - data(1, 1);
-
-close all;
 dimen = length(indices);
-figure;
-draw_data_columns(data, indices, 1.0, plot3d);
+draw_data_columns(data, indices, 1.0, plot3d, line_styles);
 if plot3d
     legend('xyz');
 else if dimen == 3
