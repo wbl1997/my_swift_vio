1aee688a39fbb917957eb141520411627221fc89
JIanzhu Huai <huai.3@osu.edu>
2020-06-04 11:46:29 +0800

    eigen quaternion do not handle left handed DCM

--- test/eigen/TestEigenMatrixInitialization.cpp
+++ test/eigen/TestEigenMatrixInitialization.cpp
@@ -1,7 +1,6 @@
 
 #include <iostream>
 #include<Eigen/Core>
-#include<Eigen/Geometry>
 #include <gtest/gtest.h>
 
 TEST(EigenMatrix, Initialization){
@@ -36,31 +35,6 @@ TEST(EigenMatrix, Initialization){
     ASSERT_NEAR((cov.block<2,2>(5,5).lpNorm<1>()), 0.08, 1e-8);
     ASSERT_NEAR((cov.block<2,3>(5,0).lpNorm<1>()), 0.08, 1e-8);
     ASSERT_NEAR((cov.block<3,2>(0,5).lpNorm<1>()), 0.08, 1e-8);
-
-}
-
-TEST(EigenMatrix, AngleAxis){
-    Eigen::Matrix3d m;
-    Eigen::Vector3d axisAngles(0.1, 0.2, 0.3);
-    axisAngles*=M_PI;
-    m = Eigen::AngleAxisd(axisAngles[0], Eigen::Vector3d::UnitX())
-            * Eigen::AngleAxisd(axisAngles[1], Eigen::Vector3d::UnitY())
-            * Eigen::AngleAxisd(axisAngles[2], Eigen::Vector3d::UnitZ());
-    Eigen::Vector3d ea = m.eulerAngles(0, 1, 2);
-    ASSERT_NEAR((ea- axisAngles).lpNorm<Eigen::Infinity>(), 0, 1e-8);
-
-    double theta= M_PI/6;
-    Eigen::Matrix3d Rx = Eigen::AngleAxisd(theta, Eigen::Vector3d::UnitX()).toRotationMatrix();
-    Eigen::Matrix3d Ry = Eigen::AngleAxisd(theta, Eigen::Vector3d::UnitY()).toRotationMatrix();
-    Eigen::Matrix3d Rz = Eigen::AngleAxisd(theta, Eigen::Vector3d::UnitZ()).toRotationMatrix();
-    Eigen::Matrix3d Rxs2f, Rys2f, Rzs2f; //rotation of angle theta from start frame to finish frame
-    double st =sin(theta), ct = cos(theta);
-    Rxs2f <<1, 0, 0, 0, ct, st, 0, -st, ct;
-    Rys2f <<ct, 0, -st, 0,1,0, st, 0, ct;
-    Rzs2f <<ct, st, 0, -st, ct, 0, 0,0,1;
-    ASSERT_NEAR((Rx- Rxs2f.transpose()).lpNorm<Eigen::Infinity>(), 0, 1e-8);
-    ASSERT_NEAR((Ry- Rys2f.transpose()).lpNorm<Eigen::Infinity>(), 0, 1e-8);
-    ASSERT_NEAR((Rz- Rzs2f.transpose()).lpNorm<Eigen::Infinity>(), 0, 1e-8);
 }
 
 TEST(EigenMatrix, VectorSize) {
