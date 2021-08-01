04ec91df126742e1633d23003e94b93468449d5a
BLWbang <1768731488@qq.com>
2021-08-02 00:59:10 +0800

    delete somme files

--- thirdparty/new/okvis_common/src/PipelineParams.cpp
+++
@@ -1,7 +0,0 @@
-#include "okvis/PipelineParams.h"
-namespace swift_vio {
-PipelineParams::PipelineParams(const std::string& name) : name_(name) {
-  CHECK_EQ(kTotalWidth, kNameWidth + kValueWidth)
-      << "Make sure these are consistent for pretty printing.";
-}
-}  // namespace swift_vio
